51e2f197229903e1b655d058c49433b9c6fdf327
munozarturo <munoz.arturoroman@gmail.com>
2023-04-24 14:20:31 -0400

    v1/25:00

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -15,14 +15,30 @@ router.get('/', async (req, res) => {
 });
 
 // Add posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
 
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+
+    res.status(201).send();
+});
 
 // Delete posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
 
+    await posts.deleteOne({
+        _id: new mongodb.ObjectId(req.params.id)
+    });
 
+    res.status(200).send();
+});
+
+// Load Posts Collection
 async function loadPostsCollection() {
-    console.log(process.env.SUDO_PASSWORD);
-
     const client = await mongodb.MongoClient.connect(uri, {
         useNewUrlParser: true
     });
