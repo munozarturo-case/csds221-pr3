585ba6d86c2d5439e2549ea7af872706e8d885a5
munozarturo <munoz.arturoroman@gmail.com>
2023-04-24 13:45:06 -0400

    v1/16:21

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,9 +3,11 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
+const uri = `mongodb+srv://sudo:${process.env.SUDO_PASSWORD}@vue-express-app.ef67yar.mongodb.net/?retryWrites=true&w=majority`;
+
 // Get posts
 router.get('/', (req, res) => {
-    res.send('Hello');
+    
 });
 
 // Add posts
@@ -13,6 +15,14 @@ router.get('/', (req, res) => {
 
 // Delete posts
 
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect(uri, {
+        useNewUrlParser: true
+    });
+
+    return client.db('vue-express').collection('posts');
+}
 
 
 module.exports = router;
