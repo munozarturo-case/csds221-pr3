8e6b67e2ffd32f834f68a6a830bd8fbe40eec2f9
munozarturo <munoz.arturoroman@gmail.com>
2023-04-25 19:08:38 -0400

    added refresh feed

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ import styles from '@/styles/Home.module.css'
 
 import UserUtil from '@/utils/user';
 
+import SyncIcon from '@mui/icons-material/Sync';
+
 export default function Home() {
   const posts = [
     {
@@ -107,20 +109,28 @@ export default function Home() {
     setUser({username: UserUtil.getRandomUsername(), likeCount: 0, postCount: 0});
   }, []);
 
+  const handleFeedRefresh = () => {
+    console.log("Feed Refreshed");
+  }
+
   return (
     <>
       <Head>
-        <title>Picshure</title>
+        <title>Thought Share - @{user.username}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="@/public/camera.png"></link>
+        <link rel="icon" href="/brain.png"></link>
       </Head>
       <header className={styles.mainHeader}>
         <Image 
-          src="/camera.png"
+          src="/brain.png"
           width={50}
           height={50}
         />
-        <h1>Picshure</h1>
+        <h1>Thought Share</h1>
+        <button onClick={handleFeedRefresh()} className={styles.refreshButton}>
+          <SyncIcon  />
+          Refresh Feed
+        </button>
       </header>
       <main className={styles.pageContent}>
         <div className={styles.content}>
